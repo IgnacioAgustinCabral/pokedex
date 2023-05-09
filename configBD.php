54ff1cfe371e0044bbbabc9d4ea75df73deb2a0c
Florencia Galvan <flgalvan@alumno.unlam.edu.ar>
2023-05-08 23:05:39 -0300

    Se agregan funciones(ListarTodo funcionando, darDeAlta(falta probar), se agrega el listadoCompleto, que es una tabla sin nada de disenio que muestra la informacion traida de base por la funcion listarTodo. Se sube carpeta con las imagenes de los pokemon agregados a la tabla. Se arregla el archivo configBD, esta funcionando correctamente.

--- configBD.php
+++ configBD.php
@@ -6,10 +6,13 @@
     $dbname = 'pokedex';
 
     // Realizar la conexión a la base de datos
-    $db = mysqli_connect($host, $user, $pass, $dbname);
+   return $db = mysqli_connect($host, $user, $pass, $dbname);
 
     // Verificar si la conexión fue exitosa
     if (!$db) {
         die('No se pudo conectar a la base de datos: ' . mysqli_connect_error());
     }
+
+
+
 ?>
