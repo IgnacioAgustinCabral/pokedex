abdbf9123d87aa651cd3b30a74db8eab76d95df4
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2023-05-09 23:32:40 -0300

    tabla con permiso de admin

--- index.php
+++ index.php
@@ -5,9 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <title>Document</title>
 </head>
+
 
 <body>
 <?php
@@ -16,27 +18,13 @@ include_once ('header.php');
 if(isset($_GET['error'])){
     echo '<h1 style="color:red">Datos Incorrectos</h1>';
 }
-?>
+if(isset($_SESSION["admin"])){
+    echo '<a style="margin-top: 2em;" href="altaPokemon.php" class="link-success mt-5 mb-5">Agregar Pokémon</a>';
+}
+require_once ('listadoCompleto.php');
 
+?>
 </body>
 <script src="js/bootstrap.min.js"></script>
-
-
-<a href="detallepokemon.php?nombre=Charmander&id=01&tipo=fuego&imagen=imgs/Charmander.png"> Ver </a>
-
-<br>
-<br>
-
 </html>
 
-
-<?php
-$id = "01";
-$nombre = "Charmander";
-$tipo = "fuego";
-$imagen = "imgs/Charmander.png";
-
-echo '<a href="detallepokemon.php?id=' . $id . '&nombre=' . $nombre . '&tipo=' . $tipo . '&imagen=' . $imagen . '">' . $nombre . '</a>';
-
-
-?>
